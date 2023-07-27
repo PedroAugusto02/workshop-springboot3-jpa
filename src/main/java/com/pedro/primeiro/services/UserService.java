9aae84e0775ab08587203f2c1a5edabd9f6ee7f3
PedroAugusto02 <pedroaugustodupas@hotmail.com>
2023-07-27 10:34:51 -0300

    Service layer, component registration

---
+++ src/main/java/com/pedro/primeiro/services/UserService.java
@@ -0,0 +1,26 @@
+package com.pedro.primeiro.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedro.primeiro.entities.User;
+import com.pedro.primeiro.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj =  repository.findById(id);
+		return obj.get();
+	}
+}
