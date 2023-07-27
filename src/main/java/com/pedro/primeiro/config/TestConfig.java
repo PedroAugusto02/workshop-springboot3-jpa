ac26b25f0faa1dee47235e400abaebe4cce2df54
PedroAugusto02 <pedroaugustodupas@hotmail.com>
2023-07-27 11:29:40 -0300

    OrderStatus enum

--- src/main/java/com/pedro/primeiro/config/TestConfig.java
+++ src/main/java/com/pedro/primeiro/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.pedro.primeiro.entities.Order;
 import com.pedro.primeiro.entities.User;
+import com.pedro.primeiro.entities.enums.OrderStatus;
 import com.pedro.primeiro.repositories.OrderRepository;
 import com.pedro.primeiro.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "41999104324", "123456");
 		User u3 = new User(null, "Ioly Oliveira", "ioly@gmail.com", "4130821991", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT ,u3);
 		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
